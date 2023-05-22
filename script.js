1746a2bfce44249e9a553d2942a3e3d8ea5c16de
Andreworld <palvinskiy@gmail.com>
2023-05-22 20:21:14 +0400

    Задание 4. Создание двух переменных 10 и 2.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let a = Number (alert(20));
 const iPhone1 = Number (alert(2007));
 const JavaScript = alert("Брендан Айком");
+let x=10;
+let y=2;
+alert (x+y);
+alert (x-y);
+alert (x*y);
+alert (x%y);
