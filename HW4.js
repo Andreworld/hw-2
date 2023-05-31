e586dd6183f60d6b9610b3cdf2df3c22877b6afe
Andreworld <palvinskiy@gmail.com>
2023-05-31 19:13:57 +0400

    Задание 4

--- HW4.js
+++ HW4.js
@@ -16,3 +16,12 @@ while (i<= 22) {
 console.log
  (i); i++;
 }
+// Задание 4
+const obj = {
+     Коля: 200,
+     Вася: 300,
+     Петя: 400,
+};
+for (let name in obj ) {
+console.log(`${name} - Зарплата ${obj[name]} Долларов`);
+}
