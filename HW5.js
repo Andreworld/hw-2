f579e4f215a32583fff0c597fd34cd5998654531
Andreworld <palvinskiy@gmail.com>
2023-06-06 19:02:35 +0400

    Задани 2

--- HW5.js
+++ HW5.js
@@ -4,10 +4,22 @@ const min = (a, b) => {
     if (a >= b) {
         return b;
     } else {
-    return a;
+        return a;
     }
 }
 undefined
 console.log(min(8, 4));
-console.log(min(6, 6))
+console.log(min(6, 6));
+
+// Задани 2 //
+
+let num = prompt('Введите число')
+function evenOrodd(a) {
+    if (a % 2 == 0) {
+        alert('Число четное');
+    } else {
+        alert('Число нечетное');
+    }
+}
+evenOrodd(num);
 
