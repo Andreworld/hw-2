b3ac6ef6a4caa967d573e5620f80f734c13bfa63
Andreworld <palvinskiy@gmail.com>
2023-06-16 15:55:41 +0400

    2.6 задание 2

--- 6HW.js
+++ 6HW.js
@@ -1,6 +1,8 @@
-const numbs = [1, 5, 4, 10, 0, 3];
+const numbs = [1, 5, 4, 10, 0, 3];  // задание 1
 
 for (let i = 0; i < numbs.length; i++) {
 	if (numbs[i] == 10) break;
 	console.log(numbs[i]);
 }
+
+[1, 5, 4, 10, 0, 3].indexOf(4)  // задание 2
