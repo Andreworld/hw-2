d58f219eddc017f0cc851cf90427037a6fbe188f
Andreworld <palvinskiy@gmail.com>
2023-06-16 18:05:36 +0400

    2.6 задание 3

--- 6HW.js
+++ 6HW.js
@@ -6,3 +6,10 @@ for (let i = 0; i < numbs.length; i++) {
 }
 
 [1, 5, 4, 10, 0, 3].indexOf(4)  // задание 2
+
+let numds = [1, 3, 5, 10, 20];
+numbs=numbs.join ('');
+console.log (numbs);
+
+const numbs = [1, 3, 5, 10, 20];    // задание 3
+console.log(numbs.join(' '));
