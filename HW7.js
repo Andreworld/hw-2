293dd4b9ace790418393f5ae9770e5f8fa6fb5e0
Andreworld <palvinskiy@gmail.com>
2023-06-21 21:24:41 +0400

    2.7 задание 3

--- HW7.js
+++ HW7.js
@@ -2,3 +2,23 @@
 
 let str = 'js'; 
 console.log(new String(str).toUpperCase());
+
+// задание 2 ????
+
+const words = ['Кошка', 'Кит', 'Комар', 'Носорог'];
+const search = 'ко';
+
+words.forEach((str) => {
+	
+	if (str.toLowerCase().includes(search.toLowerCase())) {
+		console.log(str);
+	}
+});
+
+// задание 3
+
+x = Math.floor(32.58884);
+
+x = Math.ceil(32.58884);
+
+x = Math.round(32.58884);
