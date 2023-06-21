9230ece224c17f00a56be30535557a094a52d686
Andreworld <palvinskiy@gmail.com>
2023-06-21 23:32:50 +0400

    2.7 задание 10

--- HW7.js
+++ HW7.js
@@ -53,4 +53,13 @@ let currentDate = new Date();
 currentDate.setDate (73);
 console.log(currentDate);
 
+// задание 10
+
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+let myDate = new Date(); 
+let fullDate = myDate.getDate() + " " + months[myDate.getMonth()] +
+" " + myDate.getFullYear() + " - это " + days[myDate.getDay('- это')];
+console.log(fullDate);
 
